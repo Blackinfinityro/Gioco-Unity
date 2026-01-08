ce19d9459becf67db41ee04cb1b53f68c9bb12be
Blackinfinityro <94046491+Blackinfinityro@users.noreply.github.com>
2026-01-08 09:53:10 +0100

    Movimento e animazioni
    Da aggiustare

--- Assets/Gioco/Script/Movement.cs
+++ Assets/Gioco/Script/Movement.cs
@@ -1,26 +1,55 @@
+using System.Reflection;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] public float velocità = 5f;
-    private Rigidbody2D rb;
-    private Input controls;
-    private Rigidbody2D rigidBody;
+    [SerializeField] float velocità = 5f;
+    [SerializeField] float forzaSalto = 12f;
+    [SerializeField] Transform controlloTerreno;
+    [SerializeField] LayerMask terreno;
+    Rigidbody2D rb;
+    Animator animator;
     float movimentoGiocatore;
+    float scalaInizialeX;
+    bool isGrounded;
+    bool jumpRequest;
 
-    private Vector2 moveInput;
-        void Start()
+    void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        scalaInizialeX = Mathf.Abs(transform.localScale.x);
     }
 
     void Update()
     {
-        movimentoGiocatore = controls.Movement.horizontal.ReadValue<float>();
+        movimentoGiocatore = Input.GetAxis("Horizontal");
+        animator.SetFloat("Velocità", Mathf.Abs(movimentoGiocatore));
+        Flip(movimentoGiocatore);
+        isGrounded = Physics2D.OverlapCircle(controlloTerreno.position, 0.2f, terreno);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            jumpRequest = true;
     }
 
     void FixedUpdate()
     {
-        rigidBody.linearVelocityX = velocità * movimentoGiocatore;
+        rb.linearVelocity = new Vector2(movimentoGiocatore * velocità, rb.linearVelocity.y);
+        if (jumpRequest)
+            {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, forzaSalto);
+            jumpRequest = false;
+            }
     }
+    void Flip(float direzione)
+{
+    if (direzione == 0) return;
+
+    transform.localScale = new Vector3(
+        scalaInizialeX * Mathf.Sign(direzione),
+        transform.localScale.y,
+        transform.localScale.z
+    );
 }
+
+}
+
