1681dc3189d36f1d6c2acfef9f3af29eff3fa4e6
Blackinfinityro <94046491+Blackinfinityro@users.noreply.github.com>
2026-02-05 10:01:23 +0100

    Modifica package

--- Assets/Gioco/Script/Movement.cs
+++ Assets/Gioco/Script/Movement.cs
@@ -1,23 +1,43 @@
-using System.Reflection;
 using System.Collections;
-using UnityEngine;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float velocità = 5f;
-    [SerializeField] float forzaSalto = 12f;
-    [SerializeField] Transform controlloTerreno;
-    [SerializeField] LayerMask terreno;
+    [SerializeField]
+    float velocità = 5f;
+
+    [SerializeField]
+    float forzaSalto = 12f;
+
+    [SerializeField]
+    Transform controlloTerreno;
+     [SerializeField]
+    Transform controlloMuro;
+
+    [SerializeField]
+    LayerMask terreno;
+
+    [SerializeField]
+    LayerMask wallJump;
+
+    [SerializeField]
+    PhysicsMaterial2D groundMaterial;
+
+    [SerializeField]
+    PhysicsMaterial2D noFriction;
+
     Rigidbody2D rb;
     Animator animator;
     float movimentoGiocatore;
     float scalaInizialeX;
     int runRequest;
     bool isGrounded;
+    bool isGripped;
     bool jumpRequest;
     float lastSlowDown = float.NegativeInfinity;
-    
 
     void Start()
     {
@@ -28,17 +48,22 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(controlloTerreno.position, 1f, terreno);
+        isGripped = Physics2D.OverlapCircle(controlloMuro.position, 1f, wallJump);
         movimentoGiocatore = Input.GetAxis("Horizontal");
         animator.SetFloat("Velocità", Mathf.Abs(movimentoGiocatore));
         Flip(movimentoGiocatore);
-        isGrounded = Physics2D.OverlapCircle(controlloTerreno.position, 1f, terreno);
         if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             jumpRequest = true;
-        if (Input.GetButtonDown("Run")) {
+        }
+        if (Input.GetButtonDown("Run"))
+        {
             runRequest = 1;
             Debug.Log("Run");
         }
-        if (Input.GetButtonUp("Run")) {
+        if (Input.GetButtonUp("Run"))
+        {
             runRequest = 2;
         }
     }
@@ -46,45 +71,60 @@ public class Movement : MonoBehaviour
     void FixedUpdate()
     {
         ChangeVelocity();
+        ChangeMaterial();
         rb.linearVelocity = new Vector2(movimentoGiocatore * velocità, rb.linearVelocity.y);
         if (jumpRequest)
-            {
+        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, forzaSalto);
-            
             jumpRequest = false;
-            }
+        }
         if (movimentoGiocatore == 0)
         {
             velocità = 15;
         }
     }
+
     void Flip(float direzione)
-{
-    if (direzione == 0) return;
+    {
+        if (direzione == 0)
+            return;
+        {
+            transform.localScale = new Vector3(
+                scalaInizialeX * Mathf.Sign(direzione),
+                transform.localScale.y,
+                transform.localScale.z
+            );
+        }
+    }
 
-    transform.localScale = new Vector3(
-        scalaInizialeX * Mathf.Sign(direzione),
-        transform.localScale.y,
-        transform.localScale.z
-    );
-}
-
-void ChangeVelocity()
+    void ChangeVelocity()
     {
         if (runRequest == 1 && velocità == 15)
         {
             velocità += 20;
-            
         }
         if (runRequest == 2)
         {
-            if (Time.time - lastSlowDown >= 1) {
+            if (Time.time - lastSlowDown >= 1)
+            {
                 velocità -= 2;
                 velocità = Mathf.Max(15, velocità);
                 lastSlowDown = Time.time;
             }
-        } else
+        }
+        else
             lastSlowDown = float.NegativeInfinity;
     }
-}
 
+    void ChangeMaterial()
+    {
+        if (isGrounded)
+        {
+            rb.sharedMaterial = groundMaterial;
+        }
+        if (isGripped)
+        {
+            rb.sharedMaterial = noFriction;
+        }
+    }
+}
